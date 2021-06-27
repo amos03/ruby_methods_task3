ec548d9963cdcb7020add3e6e056a62ed85060d4
amos03 <amossi@gmail.com>
2021-06-27 13:49:26 -0400

    Exercise 6 complete

--- exercise6.rb
+++ exercise6.rb
@@ -17,3 +17,43 @@ puts "#{speed(100,5)} m/s"
 puts "Speed: 250 m/ 100s"
 puts speed(250,100)
 puts "--------------------------------"
+
+Pi = 3.14159
+def circumference(r)
+    2*Pi*r
+end
+
+def sphere_volume(r)
+    4 * Pi * (r**3)/3
+    # have to ask Sree and Elvis about order of operations in Ruby (strange stuff with the fractions)
+end
+
+puts "Calculating circumference of circle"
+p circumference(5)
+p circumference(1)
+
+puts "Calculating volume of sphere"
+p sphere_volume(1)
+p sphere_volume(3)
+puts "--------------------------------"
+
+puts "Hypotenuse of a triangle"
+def hypotenuse(a,b)
+    return (a**2 + b**2)**(0.5)
+end
+
+p hypotenuse(3,4)
+p hypotenuse(9,12)
+p hypotenuse(7,7)
+puts "--------------------------------"
+
+puts "E=mc^2"
+
+C = 299792458
+
+def energy(m)
+    return m*(C**2)
+end
+
+p energy(0.05)
+p energy(0.00003)
