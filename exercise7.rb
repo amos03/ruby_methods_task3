05bde134aeefebfaf7375081c55a878ba540d7f2
amos03 <amossi@gmail.com>
2021-06-27 16:25:50 -0400

    second method for largest of 3 numbers using array

--- exercise7.rb
+++ exercise7.rb
@@ -25,3 +25,21 @@ p largest_number_below_eleven(15,8,300)
 p largest_number_below_eleven(15,300,8)
 p largest_number_below_eleven(8,8,300)
 p largest_number_below_eleven(3,15,3)
+
+puts "------------------------------------"
+
+# largest number problem using an array
+puts "Largest of 3 numbers that is 10 or below - using array:"
+def largest_number_using_array(num1=0, num2=0, num3=0)
+    three_number_array=[num1,num2,num3]
+    three_number_array.select! {|num| num <=10}
+    return three_number_array.max(1)
+end
+
+puts largest_number_using_array(1,10,9)
+puts largest_number_using_array(1,6)
+puts largest_number_using_array(100, 3)
+puts "------------------------------------"
+
+#print out provided string quantity no. of times
+puts "Print out provided string X no. of times"
